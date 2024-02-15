72fdfdde8865f84b27990a9ea45d5ed2290a7276
tarannum-2002 <tarannums@sahaj.ai>
2024-02-15 10:31:37 +0530

    initial commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,9 @@
 package org.example
 
 fun main() {
-    println("Hello World!")
+    val ticket = arrayListOf(
+        arrayListOf(4,16, Int.MAX_VALUE, Int.MAX_VALUE,48,Int.MAX_VALUE,63,76,Int.MAX_VALUE),
+        arrayListOf(7, Int.MAX_VALUE,23,38, Int.MAX_VALUE,52,Int.MAX_VALUE, Int.MAX_VALUE,80),
+        arrayListOf(9, Int.MAX_VALUE,25,Int.MAX_VALUE, Int.MAX_VALUE, 56,64,  Int.MAX_VALUE, 83 )
+    )
 }
