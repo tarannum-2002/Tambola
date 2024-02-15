f21c1a0bd2cbadffc7070a58eedc2add4b51a19f
tarannum-2002 <tarannums@sahaj.ai>
2024-02-15 10:32:31 +0530

    initial commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,4 +6,10 @@ fun main() {
         arrayListOf(7, Int.MAX_VALUE,23,38, Int.MAX_VALUE,52,Int.MAX_VALUE, Int.MAX_VALUE,80),
         arrayListOf(9, Int.MAX_VALUE,25,Int.MAX_VALUE, Int.MAX_VALUE, 56,64,  Int.MAX_VALUE, 83 )
     )
+
+    val claim = "Top Row"
+
+    val numbersAnnounced = arrayListOf(90, 4, 46, 63, 89, 16, 76, 48, 12 )
+
+
 }
